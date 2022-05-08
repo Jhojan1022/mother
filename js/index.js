81c16ba21f62c8850a898ce448c279c99c2aca08
Jhojan1022 <86871577+Jhojan1022@users.noreply.github.com>
2022-05-08 09:23:29 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,98 @@
+let imgs1 =
+    [
+        "https://scontent.fbog10-1.fna.fbcdn.net/v/t31.18172-8/11953315_1463468260629309_5387800221560155541_o.jpg?_nc_cat=101&ccb=1-6&_nc_sid=730e14&_nc_ohc=6I31lrsxutQAX8glelX&_nc_ht=scontent.fbog10-1.fna&oh=00_AT8pEWQyKqy4laWLsPElFUM_-urQ0creIKWFlGBTOq0RWg&oe=629C40E3",
+        "https://scontent.fbog10-1.fna.fbcdn.net/v/t1.6435-9/60345258_2770518346322513_6810609656507924480_n.jpg?_nc_cat=108&ccb=1-6&_nc_sid=8bfeb9&_nc_ohc=UoF2Ljm65I0AX8Qfop6&_nc_ht=scontent.fbog10-1.fna&oh=00_AT8yB1WZntClhP24kQEfjioiFC-oILFklbjFrpEWNaERgQ&oe=629C951A",
+        "https://scontent.fbog11-1.fna.fbcdn.net/v/t1.6435-9/61762731_2019643905011739_8841820194683748352_n.jpg?_nc_cat=111&ccb=1-6&_nc_sid=0debeb&_nc_ohc=06rPeESP_zUAX95H4qg&_nc_ht=scontent.fbog11-1.fna&oh=00_AT-rxaEZZj9zSFchq7XJG08TQS0y57GgI8Int8mwJHgV2A&oe=629BBF0F",
+        "https://scontent.fbog10-1.fna.fbcdn.net/v/t1.6435-9/61919384_2019643208345142_7143283525309956096_n.jpg?_nc_cat=108&ccb=1-6&_nc_sid=0debeb&_nc_ohc=VHn7AYdSURYAX_mGuoi&_nc_ht=scontent.fbog10-1.fna&oh=00_AT_E1ImjUuIIyquXe9Z5Jze_4mPWhxA5UxBHCSiXJjU6JQ&oe=629DEAB8",
+        "https://scontent.fbog10-1.fna.fbcdn.net/v/t1.6435-9/61526291_2019643155011814_4149493493804302336_n.jpg?_nc_cat=107&ccb=1-6&_nc_sid=0debeb&_nc_ohc=wZFFLi6wg_AAX96OZJe&_nc_ht=scontent.fbog10-1.fna&oh=00_AT_EXjfmKERiixctXZXn_1RUGKrFeOf9YVqH0Pdaik-1Lg&oe=629C538D",
+        "https://scontent.fbog11-1.fna.fbcdn.net/v/t1.6435-9/62198730_2019641925011937_596400472982552576_n.jpg?_nc_cat=110&ccb=1-6&_nc_sid=0debeb&_nc_ohc=J9UA695-6PsAX-tqC15&_nc_ht=scontent.fbog11-1.fna&oh=00_AT_F7v8mqZzCg85vqzgU8V62Px4MeD5qk4EnmTcBo9hvvQ&oe=629BD5E4"
+    ];
+
+let imgs2 =
+    [
+        "https://scontent.fbog11-1.fna.fbcdn.net/v/t1.6435-9/62154763_2019641348345328_5342794227933773824_n.jpg?_nc_cat=105&ccb=1-6&_nc_sid=0debeb&_nc_ohc=z9ptM0jj2W8AX8fqrWE&tn=1oBRtuGKv5G7SCGC&_nc_ht=scontent.fbog11-1.fna&oh=00_AT8pTLrMpupxTewxlvd_oLS_FRQUxE38xSa95RnVAYH96A&oe=629CB1E1",
+        "https://scontent.fbog10-1.fna.fbcdn.net/v/t1.6435-9/59983608_2770699139637767_1132926013777379328_n.jpg?_nc_cat=101&ccb=1-6&_nc_sid=8bfeb9&_nc_ohc=nHQi_k3QgVgAX-vg1JE&_nc_ht=scontent.fbog10-1.fna&oh=00_AT9Uktlphc5hnVzJjvELwgeQGykul-uk7Agz0TABPnGxdg&oe=629E6724",
+        "https://scontent.fbog10-1.fna.fbcdn.net/v/t1.6435-9/60258358_2770683309639350_3469193218401239040_n.jpg?_nc_cat=109&ccb=1-6&_nc_sid=8bfeb9&_nc_ohc=WUJYTqMY_ZcAX_qXLKO&_nc_ht=scontent.fbog10-1.fna&oh=00_AT9bljC2fBiR8DMGGobBc_Eb3dxqnD2nvPeCKHO58Cfpag&oe=629C3000",
+        "https://scontent.fbog11-1.fna.fbcdn.net/v/t1.6435-9/60314503_2770682919639389_8870089836929744896_n.jpg?_nc_cat=110&ccb=1-6&_nc_sid=8bfeb9&_nc_ohc=aEh48Ehzz4IAX-WWhOt&tn=1oBRtuGKv5G7SCGC&_nc_ht=scontent.fbog11-1.fna&oh=00_AT8G4OZTDiY65tem8pdxunoELuP6wa-FV0UYnS0Bvic_mA&oe=629EBD00",
+        "https://scontent.fbog10-1.fna.fbcdn.net/v/t1.6435-9/60349981_2770519119655769_6360005786873102336_n.jpg?_nc_cat=109&ccb=1-6&_nc_sid=8bfeb9&_nc_ohc=zJ59oJ5wC0AAX-6RX2Q&_nc_ht=scontent.fbog10-1.fna&oh=00_AT8gbhSSwVxrbDFukQUXCApU_qV_EvqwkBfgfhUZYP9Ppg&oe=629D223A",
+        "https://scontent.fbog10-1.fna.fbcdn.net/v/t1.6435-9/60193806_2770518702989144_8736219292611117056_n.jpg?_nc_cat=109&ccb=1-6&_nc_sid=8bfeb9&_nc_ohc=DO1aoB5yoxEAX-vEHHt&_nc_ht=scontent.fbog10-1.fna&oh=00_AT8JqG2zPdw5zpukuHaaL_zc8FE6eOE5ojRBHDnsUlhurA&oe=629D23DE"
+    ];
+
+
+let text = "Felicidades Mamá y gracias X2 por ser padre y madre a la misma vez. Madre. Mamá, el mejor regalo que te puedo entregar y sé que es el que más te gustará, es mi corazón y todo mi cariño. Aunque haga una montaña con regalos, no será suficiente para agradecerte todo lo que has hecho por mí. Aunque vaya al fin del mundo y te traiga el regalo más caro, sé que no podrá pagar todo lo que has hecho por mí, todo lo que has sufrido y las privaciones por las que has pasado para hacerme todo un hombre. Gracias Mamá. Para ti que siempre tuviste tus brazos abiertos y tu corazón lleno de amor hacia mí, te deseo toda la felicidad del mundo. Gracias y que pases un lindo día sabiendo que eres querida por todos. Feliz dia";
+
+
+let timer = 5000;
+let c = 1;
+
+function ChangeImg() {
+    let images = document.getElementById('images');
+    imgs1.forEach(element => {
+        setInterval(function () {
+            let img = document.createElement('img');
+            img.src = element;
+            img.id = c;
+            images.appendChild(img);
+            setTimeout(function () {
+                gsap.to(img, { duration: 2, opacity: "80%" });
+            }, 3000);
+            //gsap.to(img, {duration: 1, opacity: 0.3});
+            let position = document.getElementById(c).getBoundingClientRect();
+            window.scrollTo(0, position.top);
+            c++;
+        }, timer);
+
+    });
+
+    imgs2.forEach(element => {
+        setInterval(function () {
+            let img = document.createElement('img');
+            img.src = element;
+            images.appendChild(img);
+            setTimeout(function () {
+                gsap.to(img, { duration: 2, opacity: "80%" });
+            }, 3000);
+            //gsap.to(img, {duration: 1, opacity: 0.3});
+        }, timer);
+    });
+}
+
+let WriterC = 0;
+
+function writer() {
+    let t = document.getElementById('text');
+    let ArrFromText = text.split('');
+    console.log(ArrFromText);
+
+    setInterval(function () {
+        console.log(WriterC);
+        if (WriterC == ArrFromText.length) {
+            console.log('Finalizado');
+            t.innerHTML = "";
+            document.getElementById('message').classList.remove('message');
+            // const jsConfetti = new JSConfetti()
+            // jsConfetti.addConfetti()
+        } else {
+            if (WriterC == 211 || WriterC == 422 || WriterC == 634) {
+                console.log('siguiente pagina');
+                t.innerHTML = "";
+                t.append(ArrFromText[WriterC]);
+                WriterC++;
+            } else {
+                t.append(ArrFromText[WriterC]);
+                WriterC++;
+            }
+        }
+    }, 130);
+}
+
+
+document.getElementById('init').addEventListener('click', (e) => {
+    document.getElementById('message2').hidden = true;
+    e.hidden = true;
+    let music = document.getElementById('music');
+
+    music.play();
+    ChangeImg();
+    writer();
+});
